b9d81a6f72a1e5db799541a4c0ef8d4e5c8e28da
joaohenriquepda <joaohenrique.p.almeida@gmail.com>
2017-04-22 23:51:16 -0300

    Add controller functions

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,39 @@
+class EmployeesController < ApplicationController
+
+  before_action :set_employee, only: [:show, :update, :destroy]
+
+  # GET /employees
+  def index
+    @employees = Employee.all
+    json_response(@employees)
+  end
+
+  # GET /employees/:id
+  def show
+    json_response(@employee)
+  end
+
+  # PUT /employees/:id
+  def update
+    @todo.update(employee_params)
+    head :no_content
+  end
+
+  # DELETE /employees/:id
+  def destroy
+    @employee.destroy
+    head :no_content
+  end
+
+  private
+
+  def employee_params
+    # whitelist params
+    params.permit(:name,:registration,:password)
+  end
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+end
