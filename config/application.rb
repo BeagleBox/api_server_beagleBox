605979a4ab01ae8def62283e00152c5a87eb7e53
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2017-05-05 18:51:42 -0300

    Allowing connection with frontend via api

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module ApiServerBeaglebox
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do # As stated in https://github.com/cyu/rack-cors#common-gotchas
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :options, :delete]
+      end
+    end
   end
 end
