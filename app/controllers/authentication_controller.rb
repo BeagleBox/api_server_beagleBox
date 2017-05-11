001320045792343c0cec87a7bb0feeca9b60f240
Euler <tiagobrasil2005@yahoo.com.br>
2017-05-11 10:06:01 -0300

    return informations of employee when the login is successfull

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,7 +5,7 @@ class AuthenticationController < ApplicationController
     command = AuthenticateEmployee.call(params[:email], params[:password])
 
     if command.success?
-      render json: { auth_token: command.result }
+      render json: command 
     else
       render json: { error: command.errors }, status: :unauthorized
     end
