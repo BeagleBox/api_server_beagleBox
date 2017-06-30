4f15a2b6e0da6c0ce1a4c5d7396294b556f1f179
yeltsinlobato <yeltsin.lobato@gmail.com>
2017-06-30 15:35:30 -0300

    Include Sender and Recipient in Delivery Serializer

--- app/serializers/delivery_serializer.rb
+++ app/serializers/delivery_serializer.rb
@@ -1,5 +1,5 @@
 class DeliverySerializer < ActiveModel::Serializer
-  attributes :id, :status, :created_at, :updated_at, :tracker, :route
+  attributes :id, :status, :created_at, :updated_at, :tracker, :route, :sender, :recipient
   belongs_to :source
   belongs_to :destination
 end
