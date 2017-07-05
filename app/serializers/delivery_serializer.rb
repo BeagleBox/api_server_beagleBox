a71b0eb749a1bcb5533f881f3274ec59b9a97c5f
yeltsinlobato <yeltsin.lobato@gmail.com>
2017-07-05 16:14:04 -0300

    include items model in delivery serializer

--- app/serializers/delivery_serializer.rb
+++ app/serializers/delivery_serializer.rb
@@ -1,5 +1,5 @@
 class DeliverySerializer < ActiveModel::Serializer
-  attributes :id, :type, :status, :created_at, :updated_at, :tracker, :route, :sender, :recipient, :key_access
+  attributes :id, :type, :status, :created_at, :updated_at, :tracker, :route, :sender, :recipient, :key_access, :items
   belongs_to :source
   belongs_to :destination
 
