ad491e5576a949a68215b1c814e55a938d37d82d
yeltsinlobato <yeltsin.lobato@gmail.com>
2017-07-06 18:54:20 -0300

    Adjust Route Name

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,12 @@
 class Route < ApplicationRecord
+
+	after_create :set_route_name
+
 	belongs_to :source,      :class_name => "Departament", optional: true
 	belongs_to :destination, :class_name => "Departament", optional: true
+
+
+	def set_route_name
+   		update_column(:name, self.source.departament_name + "-" + self.destination.departament_name)
+	end
 end
