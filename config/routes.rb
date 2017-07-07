c99d309621a2516f5b37bd5997f57e76f89c59ca
yeltsinlobato <yeltsin.lobato@gmail.com>
2017-07-07 02:19:48 -0300

    Adjust Routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   post 'login', to: 'authentication#authenticate'
 
-  get '/monitor/:att', to: 'monitorings#battery_callback'
+  get '/monitor', to: 'monitorings#battery_callback'
 
   #get names of departaments as string
   get '/departament_name', to: 'departaments#departament_name'
