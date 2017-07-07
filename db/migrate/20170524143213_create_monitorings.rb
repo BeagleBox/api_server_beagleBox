e189c8077c55a2dc7791355e29203a546599fd14
yeltsinlobato <yeltsin.lobato@gmail.com>
2017-07-06 22:44:22 -0300

    Fix callback battery 2

--- db/migrate/20170524143213_create_monitorings.rb
+++ db/migrate/20170524143213_create_monitorings.rb
@@ -2,8 +2,6 @@ class CreateMonitorings < ActiveRecord::Migration[5.0]
   def change
     create_table :monitorings do |t|
       t.string  :stauts
-      t.decimal :weight
-      t.decimal :battery_percentage
       
       t.timestamps
     end
